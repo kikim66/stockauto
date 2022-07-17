522f105e16852942ab966e455e13a959c9f4ebbc
HOME-MAIN\kikim <kikim66@gmail.com>
2022-07-17 22:28:47 +0900

    init version

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+
+# slack message send code.
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = "xoxb-2074975932976-2051392356611-EYlXnI5I6RGzyoUuWbCQfYFV"
+ 
+post_message(myToken,"#stock-test","Hello , New world!! ")
+
+'''
+def dbgout(message):
+    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
+    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
+    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
+    post_message(myToken,"#stock", strbuf)
+'''
